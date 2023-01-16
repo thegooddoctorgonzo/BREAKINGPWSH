c8aa3242950e3245868fbfa6d1b3da9b05e7e6c0
Steven Landry <steven.m.landry@gmail.com>
2023-01-16 11:04:11 -0600

    20230116-1104

--- articles/20221125_Breaking Get-ChildItem Return Types/Breaking Get-ChildItem Return Types.ps1
+++ articles/20221125_Breaking Get-ChildItem Return Types/Breaking Get-ChildItem Return Types.ps1
@@ -1,55 +1,28 @@
 $gci = Get-ChildItem -Path .\README.md
-
 $gci_withname = Get-ChildItem -Path . -Name "README.md" 
-
 $gci_withfilter = Get-ChildItem -Path . | Where-Object {$_.Name -eq "README.md"} 
-
-
 
 Write-Output "`n---------------------------`nObject returned properties`n___________" 
 
-
-
 $gci 
-
 $gci_withname 
-
 $gci_withfilter 
-
-
 
 Write-Output "`nObject Fullname/PSPath`n_________" 
 
-
-
 $gci.FullName 
-
 $gci_withname.PSPath 
-
 $gci_withfilter.FullName 
-
-
 
 Write-Output "`nObject type name`n_________" 
 
-
-
 $gci.GetType().Name 
-
 $gci_withname.GetType().Name 
-
 $gci_withfilter.GetType().Name 
 
-
-
 Write-Output "`nObject base type`n__________" 
-
-
-
 Write-host "$($gci.GetType().BaseType)" 
-
 Write-host "$($gci_withname.GetType().BaseType)" 
-
 Write-host "$($gci_withfilter.GetType().BaseType)" 
 
 

dc3390380e647a6a19a8a66cee9a69ecd27fca79
Steven Landry <steven.m.landry@gmail.com>
2023-07-14 10:18:38 -0500

    20230714

--- Draft Articles/Finding function_cmdlet  params/Get Parameters for cmdlet.ps1
+++ Draft Articles/Finding function_cmdlet  params/Get Parameters for cmdlet.ps1
@@ -1,3 +1,9 @@
 (get-item Function:\Update-AllBranches).Parameters | Select-Object -ExpandProperty keys
 (Get-Command -Name Get-ChildItem).Parameters
 
+$cmdlets = Get-Command -Verb get -Module Microsoft.PowerShell.Management | select -Property *
+
+foreach($c in $cmdlets | where {$_.parameters.keys -contains "ComputerName"})
+{
+    $c.name
+}
